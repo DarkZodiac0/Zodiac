5dc1ce9d3fbb35db58ebe8220aec35d3de308a89
DarkZodiac0 <axel0030105@hotmail.com>
2025-03-07 14:44:59 -0600

    Crear Codigo.py
    codigo phyton con spyder

---
+++ Codigo.py
@@ -0,0 +1,18 @@
+import time
+
+def es_primo(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+# Solicitar al usuario que ingrese un número
+n = int(input("Ingrese un número para verificar si es primo: "))
+
+# Verificar si el número es primo
+if es_primo(n):
+    print(f"{n} es un número primo.")
+else:
+    print(f"{n} no es un número primo.")
